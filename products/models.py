9b1ceefdd1926e1281b9939527352a6d1f6a92f3
Young Soo Koh <kohys92@gmail.com>
2021-08-16 14:33:13 +0900

    Add: created products, allergy, images tables in mysql

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.fields.related import ManyToManyField
 
 # Create your models here.
 class Menu(models.Model):
@@ -14,26 +15,12 @@ class Category(models.Model):
     class Meta:
         db_table = 'categories'
 
-# 왜 Nutritions 이 Products 보다 아래에 있으면 Nutritions 이 인식이 안
-class Nutritions(models.Model):
-    one_serving_kca = models.DecimalField(max_digits=6,decimal_places=2)
-    sodium_mg = models.DecimalField(max_digits=6,decimal_places=2)
-    saturated_fat_g = models.DecimalField(max_digits=6,decimal_places=2)
-    sugars_g = models.DecimalField(max_digits=6,decimal_places=2)
-    protein_g = models.DecimalField(max_digits=6,decimal_places=2)
-    caffeine_mg = models.DecimalField(max_digits=6,decimal_places=2)
-    size_ml = models.CharField(max_length=45)
-    size_fluid_ounce = models.CharField(max_length=45)
-
-    class Meta:
-        db_table = 'nutritions'
-
 class Product(models.Model):
     korean_name = models.CharField(max_length=45)
     english_name = models.CharField(max_length=45)
-    description = models.TextField
+    description = models.TextField(blank = True)
     category = models.ForeignKey(Category,on_delete=models.CASCADE)
-    nutrition = models.ForeignKey(Nutritions,on_delete=models.CASCADE)
+    # nutrition = models.ForeignKey(Nutritions,on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'products'
@@ -47,10 +34,13 @@ class Images(models.Model):
 
 class Allergy(models.Model):
     name = models.CharField(max_length=45)
+    product = models.ManyToManyField(Product)
 
     class Meta:
         db_table = 'allergy'
 
+
+# First attempt: Creating Many to Many rel. table
 # class Allergy_products(models.Model):
 #     allergy = models.ForeignKey(Allergy, on_delete=models.CASCADE)
 #     product = models.ForeignKey(Product, on_delete=models.CASCADE)
@@ -58,3 +48,17 @@ class Allergy(models.Model):
 #     class Meta:
 #         db_table = 'allergy_products'
 
+
+# Nutrition omitted
+# class Nutritions(models.Model):
+#     one_serving_kca = models.DecimalField(max_digits=6,decimal_places=2)
+#     sodium_mg = models.DecimalField(max_digits=6,decimal_places=2)
+#     saturated_fat_g = models.DecimalField(max_digits=6,decimal_places=2)
+#     sugars_g = models.DecimalField(max_digits=6,decimal_places=2)
+#     protein_g = models.DecimalField(max_digits=6,decimal_places=2)
+#     caffeine_mg = models.DecimalField(max_digits=6,decimal_places=2)
+#     size_ml = models.CharField(max_length=45)
+#     size_fluid_ounce = models.CharField(max_length=45)
+
+#     class Meta:
+#         db_table = 'nutritions'
